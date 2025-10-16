2b191672646f745b7895d0fe506fa1cb9408cdbe
Danru-RDRGZ99 <144158038+Danru-RDRGZ99@users.noreply.github.com>
2025-10-15 21:42:45 -0600

    Modify DATABASE_URL for pg8000 compatibility
    Updated DATABASE_URL to use pg8000 driver.

--- core/db.py
+++ core/db.py
@@ -1,9 +1,20 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 
 load_dotenv()
-DATABASE_URL=os.getenv("DATABASE_URL")
+
+# ANTES:
+# DATABASE_URL = os.getenv("DATABASE_URL")
+
+# AHORA (Añadiendo +pg8000):
+DATABASE_URL = os.getenv("DATABASE_URL").replace("postgresql://", "postgresql+pg8000://")
+
+
 engine=create_engine(DATABASE_URL, pool_pre_ping=True)
-SessionLocal=sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
