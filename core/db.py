4c0ed841fbfeb2f9afa67e426245f699737ba167
Danru-RDRGZ99 <144158038+Danru-RDRGZ99@users.noreply.github.com>
2025-10-15 21:43:01 -0600

    Update db.py

--- core/db.py
+++ core/db.py
@@ -5,16 +5,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 
 load_dotenv()
-
-# ANTES:
-# DATABASE_URL = os.getenv("DATABASE_URL")
-
-# AHORA (Añadiendo +pg8000):
 DATABASE_URL = os.getenv("DATABASE_URL").replace("postgresql://", "postgresql+pg8000://")
-
-
 engine=create_engine(DATABASE_URL, pool_pre_ping=True)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
+
